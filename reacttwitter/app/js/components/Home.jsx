cc4d36d45405cd961364da3ebdeebda8f2acfccc
fpeng2 <fangyue_peng@brown.edu>
2018-02-22 19:14:35 -0500

    first edit of reacttwitter

--- reacttwitter/app/js/components/Home.jsx
+++ reacttwitter/app/js/components/Home.jsx
@@ -1,15 +1,110 @@
 import React from 'react';
+import Tweet from "./Tweet.jsx"
 
 export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+        let nowthis = this;
+        this.state = {
+            nIntervId: null,
+            loadedIDs: [],
+            setDelay: function(delay) {
+                nowthis.changeInterval(delay)
+                console.log("home.state"+nowthis.state.new_inputs)
+            },
+            tweets: [],
+        }
+    }
 
-  render() {
-      return (
-        <div>
-	   <b> Component Code Goes Here</b>
-        </div>
-    );
-  }
+    refreshTweet(){
+        let mythis = this;
+        $.get('http://ec2-18-216-120-197.us-east-2.compute.amazonaws.com:3030/feed/start', function(data, status) {
+            if (status === "success") {
+                let new_data = []
+                for (var i = 0; i < data.length; i++) {
+                    if (mythis.state.loadedIDs.includes(data[i].id_str)) {
+                        continue;
+                    }
+                    let loaded = mythis.state.loadedIDs;
+                    mythis.setState({
+                        loadedIDs: loaded.concat([data[i].id_str])
+                    })
+                    new_data.concat([<Tweet data={data[i]}/>]);
+                }
+                let len = 26 - new_data.length;
+                for (let i = 0; i < min(len, mythis.state.tweets.length); i++) {
+                    new_data.concat([mythis.state.tweets[i]]);
+                }
+                mythis.setState({
+                    tweets: new_data,
+                })
+            } else {
+                console.log("Received error:", status);
+            }
+        });
+    }
+
+    setRefreshInterval(delay) {
+        this.refreshTweet();
+        let nIntervId = setInterval(this.refreshTweet.bind(this), delay * 1000);
+        this.setState({
+            nIntervId: nIntervId,
+        })
+    }
+
+    changeInterval(delay) {
+        if (this.state.nIntervId != null) {
+            clearInterval(this.state.nIntervId);
+            this.setState({
+                nIntervId: null,
+            });
+        }
+        if (delay > 0) {
+            this.setRefreshInterval(delay);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Welcome to Swift Feed!</h1>
+                <Sidebar setDelay={this.state.setDelay}/>
+                <div className="content">
+                    <div className="tw-block-parent">
+                        {this.state.tweets}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+class Sidebar extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    handleClick(val){
+        this.props.setDelay(val);
+    }
+
+    render(){
+        let options = [3, 10, 30, 0];
+        let inputs = options.map((val) =>
+            <div>
+                <input type="radio" name="ref-intvl" value={val} id={"input"+val} onclick={this.handleClick(val)}/>
+                {val == 0? "never" : val+"seconds"}
+                <br/>
+            </div>)
+        return (
+            <div className="config">
+                <form id="intervals">
+                    <p>Refresh every</p>
+                    {inputs}
+                </form>
+            </div>
+        );
+    }
+    //<button className="go" onclick={this.handleClick()}>change</button>
 }
--- reacttwitter/app/js/components/Tweet.jsx
+++ reacttwitter/app/js/components/Tweet.jsx
@@ -1,1 +1,56 @@
 // Create a new React component here!
+
+import React from 'react';
+
+export default class Tweet extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        let verified = this.props.data.user.verified?
+            (<span className="Icon Icon--verified"> </span>) : null;
+        let avatar = null;
+        if (this.props.data.user.hasOwnProperty("profile_image_url")) {
+            avatar = (
+                <div className="Avatar"
+                     style={"background: url(" + this.props.data.user.profile_image_url + ");"}>
+                </div>
+            )
+        } else {
+            avatar = (
+                <div className="Avatar"
+                     style={"background: url(img/no_photo.png);"}>
+                </div>
+            )
+        }
+        let authorlink = null;
+        if (this.props.data.user.hasOwnProperty("url") && this.props.data.user.url != null) {
+            console.log(item.user.url);
+            authorlink =(<a className="TweetAuthor-link" href={this.props.data.user.url}> </a>)
+        } else {
+            authorlink =(<a className="TweetAuthor-link" href="#"> </a>)
+        }
+        return (
+            <div className="timeline-TweetList-tweet">
+                <div className="timeline-Tweet">
+                    <div className="timeline-Tweet-brand">
+                        <div className="Icon Icon--twitter"></div>
+                    </div>
+                    <div className="timeline-Tweet-author">
+                        <div className="TweetAuthor">
+                            {authorlink}
+                            <span className="TweetAuthor-avatar">
+                                {avatar}
+                                </span>
+                            <span className="TweetAuthor-name">{this.props.data.user.name}</span>
+                            {verified}
+                            <span class="TweetAuthor-screenName">{"@" + this.props.user.screenName} </span>
+                        </div>
+                    </div>
+                    <div class="timeline-Tweet-text">{this.props.data.text}</div>
+                </div>
+            </div>
+        );
+    }
+}
