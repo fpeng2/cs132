b268de8258dac40bcba1f888dee6381b03a7b042
Fangyue Peng <christiful@live.cn>
2018-03-06 23:24:49 -0500

    lab5 draft

---
+++ lab5/README.md
@@ -0,0 +1,5 @@
+#Lab5
+
+##'Related' Criteria
+If two artists have a disk with the same title, then they are related.
+If a CD contains both artists' song, the two artists are related.
--- lab5/index.js
+++ lab5/index.js
@@ -1,2 +1,66 @@
 // TODO: your code goes here
 console.log(process.argv);
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var db = mongoose.connection;
+
+var CDSchema = new Schema({
+    diskid: String,
+    title: String,
+    artist: String,
+    length: Number,
+    genre: String,
+    year: Number,
+    tracks: [{
+        name: String,
+        artist: String,
+        length: Number,
+        number: Number,
+        offset: Number
+    }]
+});
+
+var CD = mongoose.model('cd', CDSchema);
+
+db.on('error', console.error); // log any errors that occur
+
+// bind a function to perform when the database has been opened
+db.once('open', function() {
+    // perform any queries here, more on this later
+    console.log("Connected to DB!");
+});
+
+// process is a global object referring to the system process running this
+// code, when you press CTRL-C to stop Node, this closes the connection
+process.on('SIGINT', function() {
+    db.close(function () {
+        console.log('DB connection closed by Node process ending');
+        process.exit(0);
+    });
+});
+
+// the user, password, and url values will be explained next
+var url = "mongodb://bdognom.cs.brown.edu/cdquery"; // 1%:
+//var url = "mongodb://bdognom.cs.brown.edu/cdquery1"; // 1%
+var options = {
+    user: 'cs132',
+    pass: 'csci1320',
+    useMongoClient: true  // due to version issues, this is necessary to avoid warnings
+};
+
+mongoose.Promise = Promise; // use an updated promise library
+mongoose.connect(url, options); // unlike the prelab we are passing in some options
+
+function listcallback(err, data) {
+    for (var i = 0; i < data.length; i++) {
+        console.log(data[i]);
+    }
+}
+
+CD.find({'tracks.artist': 'Taylor Swift'}).
+    and({'tracks.artist': 'Jay Chou'}).
+    exec(listcallback);
+
+db.close(function() {
+    console.log("program exiting");
+})
